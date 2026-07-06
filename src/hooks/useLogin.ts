0e6ee4cf34f39774ce3fae0e4fe984c3967ef131
Omer <kocartahir@gmail.com>
2026-07-06 08:58:09 -0400

    useLogin hook

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,17 @@
+﻿import { useMutation } from '@tanstack/react-query';
+import apiClient, { TOKEN_KEY } from '../api/client'
+import type { LoginDto, LoginResponse} from '../types/auth';
+
+async function loginRequest(credentials: LoginDto): Promise<LoginResponse> {
+    const response = await apiClient.post<LoginResponse>('/api/auth/login', credentials);
+    return response.data;
+}
+
+export function useLogin() {
+    return useMutation({
+        mutationFn: loginRequest,
+        onSuccess: (data) => {
+            localStorage.setItem(TOKEN_KEY, data.token);
+        }
+    })
+}
